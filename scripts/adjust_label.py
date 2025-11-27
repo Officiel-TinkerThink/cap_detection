142f51200452553f8327dd01ce28dbbfbfd2da4b
Officiel-TinkerThink <bornouksyn497@gmail.com>
2025-11-27 17:35:34 +0700

    feat: adjust_label.py

--- scripts/adjust_label.py
+++ scripts/adjust_label.py
@@ -1,89 +1,57 @@
-import argparse
 import glob
-import logging
-import os
 from pathlib import Path
-
-import cv2
-import numpy as np
-import pandas as pd
-
+import re
 
 class Relabeler:
-    def __init__(self, idx2label, labels_value):
-        self.idx2label = idx2label
-        # self.label2idx = {value:key for key, value in self.idx2label.items()}
-        self.labels_value = labels_value
+    def __init__(self, code2idx: dict[str, int], source: str, target: str):
+        """
+        Initialization of Relabeler Class
+        
+        Args:
+          code2idx (dict[str, int]): the mapping from code to index 
+          source (str): the path to the source directory 
+          target (str): the path to the target directory 
+        """
+        self.code2idx = code2idx
+        self.source = source
+        self.target = target
 
-    def classify(self, array):
-        hsv = cv2.cvtColor(array, cv2.COLOR_BGR2HSV)
-        mean_h = hsv[:, :, 0].mean()
-        mean_s = hsv[:, :, 1].mean()
-        mean_v = hsv[:, :, 2].mean()
-        if 75 <= mean_h <= 130 and mean_s > 40:
-            if mean_v > 100:
-                label = 0  # light blue
-            else:
-                label = 1  # dark blue
-        else:
-            label = 2
-        # labeling based on the treshold
-        return label, (mean_h, mean_s, mean_v)
+        if self.target:
+            Path(self.target).mkdir(parents=True, exist_ok=True)
 
-    def _process(self, image, bboxes):
-        height, width = image.shape[:2]
-        data = []
+    def classify(self, file_name:str) -> None:
+        """
+        Classify the label file
+        
+        Args:
+          file_name (str): the name of the label file
+        """
+        match = re.search(r"_b(\d+)_", file_name)
+        code = match.group(1)
+        idx = self.code2idx[code]
+        with open(Path(self.source, file_name), "r", encoding="utf-8") as f_in:
+            lines = f_in.readlines()
+      
+        modified_lines = []
+        # changed the first char of every lines
+        for line in lines:
+            if line.strip():
+                modified_line = str(idx) + line[1:]
+                modified_lines.append(modified_line)
 
-        # for each bboxes, crop the image
-        for i, bbox in enumerate(bboxes):
-            # crop the image based (get the numpy array) on the bbox given
-            x_min = int((bbox[1] - bbox[3] / 2) * width)
-            x_max = int((bbox[1] + bbox[3] / 2) * width)
-            y_min = int((bbox[2] - bbox[4] / 2) * height)
-            y_max = int((bbox[2] + bbox[4] / 2) * height)
+        with open(Path(self.target, file_name), 'w') as f_out:
+          f_out.writelines(modified_lines)  
 
-            log.debug(f"box: {bbox}")
-            log.debug(f"array: {i} --> {(x_min, y_min), (x_max, y_max)}")
-
-            box_array = image[y_min:y_max, x_min:x_max, :]
-            label, (h, s, v) = self.classify(box_array)
-
-            log.debug(f"{i} ---> {label}")
-            log.info(f"{label == bbox[0]} --> pred: {label}, default: {bbox[0]}")
-            log.info(f"h: {h}, s: {s}, v: {v}")
-            data.append(
-                {"default_label": bbox[0], "pred": label, "h": h, "s": s, "v": v}
-            )
-        return data
-
-    def process(self, image_dir):
-        overall_data = []
-        for im_path in sorted(glob.glob(f"{image_dir}/*.jpg")):
-            label_path = im_path.replace(".jpg", ".txt")
-            labels = np.loadtxt(label_path, delimiter=" ")
-            image = cv2.imread(im_path)
-            datum = labeler._process(image, labels)
-            overall_data.append(datum)
-
-        df = pd.DataFrame(overall_data)
-        df.to_csv("hsv_analysis.csv", sep=",", index=False, header=df.columns)
+    def process(self) -> None:
+        """
+        Process the label files
+        """
+        for im_path in sorted(glob.glob(f"{self.source}/*.jpg")):
+            label_file = Path(im_path.replace(".jpg", ".txt")).name
+            self.classify(label_file)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--debug", action="store_true")
-    args = parser.parse_args()
-    print(args.debug)
-    if args.debug:
-        logging.basicConfig(
-            level="DEBUG",
-            format="%(asctime)s - %(levelname)s - %(message)s",  # Define the log message format)
-        )
-    else:
-        logging.basicConfig(
-            level="INFO",
-            format="%(asctime)s - %(levelname)s - %(message)s",  # Define the log message format)
-        )
-    log = logging.getLogger()
-    labeler = Relabeler("k", 1)
-    labeler.process("sample")
+    code2idx = {"2":2, "3":2, "4":0, "5":1}
+    labeler = Relabeler(code2idx, 'sample', 'sample')
+    labeler.process()
